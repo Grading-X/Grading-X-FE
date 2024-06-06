ab0ae1b8bda679f92848ac0db77dee16c7faac29
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-06-06 16:38:46 +0900

    [Feat]: 시험결과 UI

--- src/components/result/ExamResultBox.tsx
+++ src/components/result/ExamResultBox.tsx
@@ -1,5 +1,5 @@
 import {useQuery} from "@tanstack/react-query";
-import {DASHBOARD, MINUTE_5, RESULT} from "@/const/data.ts";
+import {DASHBOARD, RESULT} from "@/const/data.ts";
 import {Result, ResultForInstructorResponse} from "@/api/result/result.response.ts";
 import {getResultForInstructorByExamId} from "@/api/result/resut.api.ts";
 import {useEffect, useState} from "react";
@@ -14,15 +14,13 @@ export default function ExamResultBox({examId, forceUpdateFlag}: Props) {
   const {data, refetch} = useQuery<ResultForInstructorResponse, Object, ResultForInstructorResponse, [_1: string, _2: string, _3: number]>({
     queryKey: [DASHBOARD, RESULT, examId],
     queryFn: getResultForInstructorByExamId,
-    staleTime: MINUTE_5,
+    staleTime: 1000,
   });
 
   const results = data?.resultInstructorResponses ?? [];
 
   useEffect(() => {
-    if(forceUpdateFlag){
-      refetch();
-    }
+    refetch();
   }, [forceUpdateFlag]);
 
 
@@ -36,17 +34,21 @@ export default function ExamResultBox({examId, forceUpdateFlag}: Props) {
           결과가 없습니다.
         </div> :
         <>
-          <div className="flex flex-row pr-20 pl-20 w-full border">
-            <div className="flex-[2]">
-              이메일
+          <div className="border rounded w-full p-1 my-4">
+            <h1 className="mt-2 ml-2 text-2xl font-medium mb-4">결과 {results.length}개</h1>
+            <div className="flex flex-row pr-20 pl-20 w-full">
+              <div className="flex-[2]">
+                이메일
+              </div>
+              <div className="flex-[1]">
+                점수
+              </div>
+              <div className="flex-[1]">
+                총점
+              </div>
+              <div className="flex-[1]"/>
             </div>
-            <div className="flex-[1]">
-              점수
-            </div>
-            <div className="flex-[1]">
-              총점
-            </div>
-            <div className="flex-[1]"/>
+
           </div>
           {
             results.map((result) => (
@@ -76,7 +78,7 @@ function AnswerItem({result}: AnswerItemProps) {
   }
   return (
     <div className="flex flex-col w-full">
-      <div className="flex flex-row justify-between pr-20 pl-20 w-full">
+      <div className="flex flex-row justify-between pr-20 pl-20 w-full border rounded py-1 my-1 bg-gray-50">
         <div className="flex-[2]">{result.email}</div>
         <div className="flex-[1]">{result.totalScore}</div>
         <div className="flex-[1]">{result.totalWeightage}</div>
@@ -85,9 +87,9 @@ function AnswerItem({result}: AnswerItemProps) {
       </div>
 
       {selected && result.gradingResponseList.map((grading) => (
-        <div key={grading.answerId} className="p-2 border-2 m-1">
-          <div>문제 : {grading.query}</div>
-          <div>답변 : {grading.answer}</div>
+        <div key={grading.answerId} className="p-2 pl-4 border rounded m-1">
+          <div className="font-medium text-[20px] mb-4">{grading.query}</div>
+          <div className="font-normal  text-gray-500 underline">{grading.answer}</div>
           <div>점수 : {grading.score}</div>
         </div>
       ))}
